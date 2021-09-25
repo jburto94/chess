ce089f2c42f72b6eafc73ca6e476b0ca29d9d818
Jake Burton <jburto94@yahoo.com>
2021-09-25 11:14:20 -0400

    Gave basic methods to piece

--- pieces/piece.rb
+++ pieces/piece.rb
@@ -1,5 +1,33 @@
-class Piece
+require_relative '../board'
 
+class Piece
+  attr_reader :board, :color
+  attr_accessor :pos
+
+  def initialize(color, board, pos)
+    @color, @board, @pos = color, board, pos
+  end
+
+  def to_s
+    " #{symbol} "
+  end
+
+  def empty?
+    false
+  end
+
+  def symbol
+    raise NotImplementedError
+  end
+
+  def valid_moves
+
+  end
+
+  private
+  def move_into_check?(end_pos)
+
+  end
 end
 
 class Rook < Piece
@@ -27,5 +55,5 @@ class Pawn < Piece
 end
 
 class NullPiece < Piece
-
+  
 end
