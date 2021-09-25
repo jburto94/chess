0305876e1982e65eb66e6828dfebe36269ce3fdc
Jake Burton <jburto94@yahoo.com>
2021-09-25 11:36:46 -0400

    Added stepable and slideable module

---
+++ pieces/slideable.rb
@@ -0,0 +1,55 @@
+module Slideable
+  HORIZONTAL_DIRS = [
+    [0,1],
+    [0,-1],
+    [1,0],
+    [-1,0]
+  ]
+
+  DIAGONAL_DIRS = [
+    [1,1],
+    [1,-1],
+    [-1,1],
+    [-1,-1]
+  ]
+
+  def horizontal_dirs
+    HORIZONTAL_DIRS
+  end
+
+  def diagonal_dirs
+    DIAGONAL_DIRS
+  end
+
+  def moves
+    moves = []
+
+    move_dirs.each { |x,y| moves += grow_unblocked_moves_in_dir(x,y) }
+
+    moves
+  end
+
+  private
+  def move_dirs
+    raise NotImplementedError
+  end
+
+  def grow_unblocked_moves_in_dir(x,y)
+    curr_x, curr_y = pos
+    moves = []
+
+    loop do
+      curr_x, curr_y = curr_x + x, curr_y + y
+      pos = [curr_x, curr_y]
+
+      break unless board.valid_pos?(pos)
+
+      if board.empty?(pos)
+        moves << pos
+      else
+        moves << pos if board[pos].color != color
+      end
+
+    moves
+  end
+end
---
+++ pieces/stepable.rb
@@ -0,0 +1,21 @@
+module Stepable
+  def moves
+    moves = []
+
+    move_diffs.each do |x,y|
+      curr_x, curr_y = pos
+      new_pos = [curr_x + x, curr_y + y]
+
+      next unless baord.valid_pos?(new_pos)
+      
+      moves << new_pos if board.empty? || board[new_pos].color != color
+    end
+
+    moves
+  end
+
+  private
+  def move_diffs
+    raise NotImplementedError
+  end
+end
